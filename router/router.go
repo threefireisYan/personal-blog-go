7a7ff3539f570d21cd4430666dd8d5d5f88d8232
threefireisYan <48510432+threefireisYan@users.noreply.github.com>
2023-05-24 10:48:52 +0800

    增加七牛云秘钥

--- router/router.go
+++ router/router.go
@@ -17,7 +17,7 @@ func Router() {
 	http.HandleFunc("/writing", views.HTML.Writing)
 	http.HandleFunc("/api/v1/post", api.API.SvarAndUpdatePost)
 	http.HandleFunc("/api/v1/post/", api.API.GetPost)
-	//http.HandleFunc("/api/v1/qiniu/token", api.API.QiniuToken)
+	http.HandleFunc("/api/v1/qiniu/token", api.API.QiniuToken)
 	http.HandleFunc("/api/v1/login", api.API.Login)
 	http.Handle("/resource/", http.StripPrefix("/resource/", http.FileServer(http.Dir("public/resource/"))))
 }
